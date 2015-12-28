8417b789860202dd6dc4836ef3bdcfb4ec0aac94
Joseph Wynn <joseph@wildlyinaccurate.com>
2015-12-28 20:50:58 +0000

    Tidy up with Map#update

--- test/MapSpec.js
+++ test/MapSpec.js
@@ -13,23 +13,28 @@ describe('Map', () => {
   it('is deeply immutable', () => {
     const map1 = new Map({
       foo: {
-        bar: 1,
         baz: {
           bing: 1
         }
       }
     })
 
-    const map2 = map1.setIn(['foo', 'bar'], 2)
-    const map3 = map1.setIn(['foo', 'baz', 'bing'], 3)
+    const map2 = map1.setIn(['foo', 'baz', 'bing'], 3)
 
-    expect(map1.getIn(['foo', 'bar'])).toBe(1)
     expect(map1.getIn(['foo', 'baz', 'bing'])).toBe(1)
+    expect(map2.getIn(['foo', 'baz', 'bing'])).toBe(3)
+  })
 
-    expect(map2.getIn(['foo', 'bar'])).toBe(2)
-    expect(map2.getIn(['foo', 'baz', 'bing'])).toBe(1)
-    expect(map3.getIn(['foo', 'baz', 'bing'])).toBe(3)
-    expect(map3.getIn(['foo', 'bar'])).toBe(1)
+  describe('.update()', () => {
+    it('should update the key with the given function', () => {
+      const map1 = new Map({ a: 1 })
+      const updater = x => x + 1
+
+      const map2 = map1.update('a', updater)
+
+      expect(map1.get('a')).toBe(1)
+      expect(map2.get('a')).toBe(2)
+    })
   })
 
 })
